7638e8ba2cca6cd7a7da683e0e1f64a42e9d0053
Lauria Rubega <112666834+Lauria12@users.noreply.github.com>
2023-05-24 23:08:53 +0200

    Adding the main to use JSON interchange data for book inventory

--- BookInventory.java
+++ BookInventory.java
@@ -25,5 +25,27 @@ import java.util.Map;
             return bookCatalogue.getOrDefault(book, 0);
         }
 
+
+        public static void main(String[] args) {
+            // Create a BookInventory instance
+            BookInventory bookInventory = new BookInventory();
+
+// Add books to the inventory
+            bookInventory.addBook(new Book("1", "Book 1", "Author 1", 2021), 5);
+            bookInventory.addBook(new Book("2", "Book 2", "Author 2", 2022), 3);
+
+// Serialize the book inventory to JSON
+            String json = bookInventory.serializeInventoryToJson();
+            System.out.println(json);
+
+// Deserialize the JSON back into the book inventory
+            bookInventory.deserializeInventoryFromJson(json);
+
+// Retrieve the quantity of a book
+            int quantity = bookInventory.getBookQuantity(new Book("1", "Book 1", "Author 1", 2021));
+            System.out.println("Quantity: " + quantity);
+
+        }
+
     }
 
